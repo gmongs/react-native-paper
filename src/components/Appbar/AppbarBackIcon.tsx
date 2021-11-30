47e65a23c9ed2b7b671d8e22ac0b3b484cadcfaf
gmongs <mail@gmong.com>
2021-11-30 12:50:53 +0900

    Update AppbarBackIcon.tsx

--- src/components/Appbar/AppbarBackIcon.tsx
+++ src/components/Appbar/AppbarBackIcon.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { Platform, I18nManager, View, Image, StyleSheet } from 'react-native';
-import Ionicons from 'react-native-vector-icons/Ionicons';
+import MaterialCommunityIcon from '../MaterialCommunityIcon';
 
 const AppbarBackIcon = ({ size, color }: { size: number; color: string }) => {
   const iosIconSize = size - 3;
@@ -25,8 +25,8 @@ const AppbarBackIcon = ({ size, color }: { size: number; color: string }) => {
       />
     </View>
   ) : (
-    <Ionicons
-      name="md-arrow-back-outline"
+    <MaterialCommunityIcon
+      name="arrow-left"
       color={color}
       size={size}
       direction={I18nManager.isRTL ? 'rtl' : 'ltr'}
